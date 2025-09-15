4a0713f9dae6266752ca6ccc5dbcf7403f441e40
Joseph <jpraposo2027@gmail.com>
2025-09-14 22:01:30 -0400

    add player health script

--- 2D Tower Defense Dice Roll/Assets/Scripts/PlayerHealth.cs
+++ 2D Tower Defense Dice Roll/Assets/Scripts/PlayerHealth.cs
@@ -1,10 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerHealth : MonoBehaviour
 {
     public float Health;
     public float maxHealth;
-    public float minHealth; 
+    public Image HealthBar; 
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
